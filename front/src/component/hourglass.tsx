454498e7be7fbf50d85d71264444dd1c581af2cf
t_takumaru <t.takuma.saga.u@gmail.com>
2025-04-08 22:55:45 +0900

    砂時計コンポーネントを作成

---
+++ front/src/component/hourglass.tsx
@@ -0,0 +1,10 @@
+import styles from './hourglass.module.css'
+
+export const HourglassTimer = () => {
+
+  return (
+    <div>HourglassTimer
+        <div className={styles.dot}></div>
+    </div>
+  );
+};
